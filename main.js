0ff40da4d7b2841c19756ffb69e01faa60d3060e
SOFYALARA <T.SOFIA@ESCOLA.PR.GOV.BR>
2024-11-14 12:07:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-
 const nomes = [“Ana”, “Fernanda”, “Maria Eduarda”,
-“Marcelo”, “Amanda”, “Gustavo”, “Gabriel”];
-
-const nomes = [“Ana”, “Fernanda”, “Maria Eduarda”,
+“Marcelo”, “Amanda”, “Gustavo”, “Gabrielconst nomes = [“Ana”, “Fernanda”, “Maria Eduarda”,
 “Marcelo”, “Amanda”, “Gustavo”, “Gabriel”];
 
 export function aleatorio (lista){
 const posicao = Math.floor(Math.random()* lista.length);
 return lista[posicao];
 }
-export const nome = aleatorio(nomes);
+export const nome = aleatorio(nomesimport { aleatorio, nome } from ‘./aleatorio.js’;function mostraResultado() {
+caixaPerguntas.textContent = `Em 2049, ${nomefunction substituiNome() {
+for (const pergunta of perguntas) {
+pergunta.enunciado = pergunta.enunciado.replace(/você/g,
+nomefunction substituiNome() {
+for (const pergunta of perguntas) {
+pergunta.enunciado = pergunta.enunciado.replace(/você/g,
+nome);
+}
+}
+substituiNome();
+mostraPergunta();
